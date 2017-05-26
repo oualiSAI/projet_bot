8ac09c00ec02cc4da98b5451e20d646278f77797
oualiSAI <wa3li193@gmail.com>
2017-05-27 00:36:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,46 @@ else if(message.content.search("!meteo")!=-1)
 			}).catch(console.log); 
 			
 }		
+else if(message.content.search("!image")!=-1)
+{var https = require('https');
+var z=message.content.search("!image");
+var x="!image".length;
 
+var titre_image=message.content.substring(z+x,message.content.length)
+var options = {
+  hostname: 'api.imgur.com',
+  path: '/3/gallery/search/time/1/?q='+titre_image,
+  headers: {'Authorization': 'Client-ID 4a23c71db2902ae'},
+  method: 'GET'
+};
+data=""
+
+ 
+
+var req = https.request(options, function(res) {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+    console.log('data:', res.data)
+
+  res.on('data', function(d) {
+    data+=d
+    
+  });
+  res.on("end", function () {
+    re=JSON.parse(data)
+        message.reply(re['data'][0]["link"]));
+    });
+});
+
+req.on('error', function(e) {
+  console.error(e);
+});
+
+req.end();
+}
+	
+	
+	
 else message.reply("Bonjour , désolé j'ai rien compris");
 
 
